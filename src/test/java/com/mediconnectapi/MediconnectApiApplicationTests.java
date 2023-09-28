05267bcb2e697b31891097e362502fd046bd7956
Maki CAMARA <camara.maki@gmail.com>
2023-09-28 22:43:15 +0200

    update : remove empty test for compiling purpose

--- src/test/java/com/mediconnectapi/MediconnectApiApplicationTests.java
+++ src/test/java/com/mediconnectapi/MediconnectApiApplicationTests.java
@@ -1,13 +1,6 @@
 package com.mediconnectapi;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class MediconnectApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
